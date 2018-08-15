4f92bd509b3d982db854b73375c24e81cb9a3559
Kevin Wu <kwu.uic@gmail.com>
2018-08-15 19:27:27 -0400

    implementing hibernate

---
+++ Project2/src/main/java/com/revature/dao/NewBatchDao.java
@@ -0,0 +1,7 @@
+package com.revature.dao;
+
+import com.revature.model.BatchAnnotation;
+
+public interface NewBatchDao {
+	public BatchAnnotation findBatchById(int id);
+}
--- Project2/src/test/java/com/revature/pagefactory/LoginPage.java
+++ Project2/src/test/java/com/revature/pagefactory/LoginPage.java
@@ -4,11 +4,17 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginPage {
   
+	public static WebDriverWait wait;
+	
 	public LoginPage (WebDriver driver) {
 		PageFactory.initElements(driver,  this);
+		wait = new WebDriverWait(driver, 30);
+		wait.until(ExpectedConditions.elementToBeClickable(login));
 	}
 	
 	@FindBy (name = "email")
@@ -19,4 +25,6 @@ public class LoginPage {
 	
 	@FindBy (name = "submit")
 	public WebElement login;
+	
+
 }
