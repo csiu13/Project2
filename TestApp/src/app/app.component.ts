c78449ef1da08b96f7547c7585d8a5fa67df7d29
Kevin Wu <kwu.uic@gmail.com>
2018-08-16 10:59:55 -0400

    testing servlets and angular

--- TestApp/src/app/app.component.ts
+++ TestApp/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'TestApp';
+  title = "testing";
+  
+  constructor (private http:HttpClient) { }
+  
+  post () {
+    return this.http.post("http://localhost:8181/test/do", (new Headers({ 'Content-Type': 'text/plain' })));
+  }
+  // public test () {
+  //   return this.http.post("http://localhost:8181/test/do", (new Headers({ 'Content-Type': 'text/plain' })));
+  // }
+
 }
+
