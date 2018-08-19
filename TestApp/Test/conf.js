ff2da2643fa4c112c686ee79f24b8c28fca0ecba
Levin Huang <levinhuang6@gmail.com>
2018-08-19 02:19:49 -0400

    protractor functionality

---
+++ TestApp/Test/conf.js
@@ -0,0 +1,13 @@
+var HtmlReporter = require('protractor-beautiful-reporter')
+
+exports.config = {
+  framework: 'jasmine',
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['spec.js'],  
+  onPrepare: function() {
+    // Add a screenshot reporter and store screenshots to `/tmp/screenshots`:
+    jasmine.getEnv().addReporter(new HtmlReporter({
+       baseDirectory: 'tmp/screenshots'
+    }).getJasmine2Reporter());
+ }
+}
